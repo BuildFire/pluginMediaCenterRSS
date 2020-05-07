f6a7e483dfa7196d3cf6cd0a41abe40760d8e614
bash <bash.kd@gmail.com>
2020-05-07 04:26:00 +0300

    fix gulp build issue

--- widget/cache.js
+++ widget/cache.js
@@ -1,10 +1,10 @@
 var cache = {
   storeName: 'cacheStore',
   saveCache: function(object) {
-    window.buildfire.localStorage.setItem(`${this.storeName}_${window.buildfire.getContext().instanceId}`, JSON.stringify(object), () => {});
+    window.buildfire.localStorage.setItem(this.storeName + '_' + window.buildfire.getContext().instanceId, JSON.stringify(object), function(){});
   },
   getCache: function(callback) {
-    window.buildfire.localStorage.getItem(`${this.storeName}_${window.buildfire.getContext().instanceId}`, (error, value) => {
+    window.buildfire.localStorage.getItem(this.storeName + '_' + window.buildfire.getContext().instanceId, function(error, value){
       if (error) return callback(error, null);
       callback(null, JSON.parse((typeof value === 'undefined') ? null : value));
     });
