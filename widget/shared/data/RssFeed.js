30feeb2faabc55e23f7d9f1a094ac33b6c9ae733
alaa smadi <alaasmadi1010@gmail.com>
2024-03-20 23:33:24 +0300

    feat(datastore): include data structure
    include data structure for feed objects

---
+++ widget/shared/data/RssFeed.js
@@ -0,0 +1,29 @@
+
+class RssFeed {
+    constructor(data = {}) {
+        this.type = "rss";
+        this.subtitle = "RSS Feed";
+        this.id = data.id || "";
+        this.title = data.title || "";
+        this.url = data.url || "";
+
+        this.advancedConfig = this.setupSettings(data.advancedConfig ? data.advancedConfig : {});
+    }
+
+    setupSettings(settings = {}) {
+        if (!settings.searchEngineItemConfig) {
+            settings.searchEngineItemConfig = {};
+        }
+        return {
+            enableSearchEngineConfig: typeof settings.enableSearchEngineConfig === 'boolean' ? settings.enableSearchEngineConfig : false,
+            searchEngineItemConfig: {
+                uniqueKey: typeof settings.searchEngineItemConfig.uniqueKey !== 'undefined' ? settings.searchEngineItemConfig.uniqueKey : "id",
+                titleKey: typeof settings.searchEngineItemConfig.titleKey !== 'undefined' ? settings.searchEngineItemConfig.titleKey : "title",
+                urlKey: typeof settings.searchEngineItemConfig.urlKey !== 'undefined' ? settings.searchEngineItemConfig.urlKey : "link",
+                descriptionKey: typeof settings.searchEngineItemConfig.descriptionKey !== 'undefined' ? settings.searchEngineItemConfig.descriptionKey : "media:group.media:keywords",
+                publishDateKey: typeof settings.searchEngineItemConfig.publishDateKey !== 'undefined' ? settings.searchEngineItemConfig.publishDateKey : "pubDate",
+                imageUrlKey: typeof settings.searchEngineItemConfig.imageUrlKey !== 'undefined' ? settings.searchEngineItemConfig.imageUrlKey : "media:group.media:thumbnail.$.url"
+            }
+        }
+    }
+}
